4fd30da76a109931dd5aab3a3b26bffbca204b72
Emile <emile.vianneyliaud@gmail.com>
2022-03-18 15:38:08 +0100

    adding Titanic project

---
+++ TITANIC/src/index.js
@@ -0,0 +1,38 @@
+import * as d3 from 'd3';
+
+// C'est ici que vous allez écrire les premières lignes en d3!
+
+const body = d3.select("body");
+
+const maxWidth = window.innerWidth;
+const maxHeight = window.innerHeight;
+console.log(maxHeight);
+
+const titanicWaveBg = document.getElementById('titanic-wave-bg');
+titanicWaveBg.
+
+// const ship = document.querySelector('#titanic-ship')
+// const waveFg = document.querySelector('#titanic-wave-fg')
+// const waveBg = document.querySelector('#titanic-wave-bg')
+
+// const tl = new TimelineMax()
+
+// tl.to(ship, 3, {
+//   rotation: -10,
+//   transformOrigin: 'center',
+//   ease: Power1.easeInOut,
+//   repeat: -1,
+//   yoyo: true
+// }, 0)
+
+// tl.to(waveFg, 3, {
+//   x: -323.766,
+//   ease: Power0.easeNone,
+//   repeat: -1
+// }, 0)
+
+// tl.from(waveBg, 3, {
+//   x: -323.766,
+//   ease: Power0.easeNone,
+//   repeat: -1
+// }, 0)
